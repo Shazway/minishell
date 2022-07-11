bcdea1598ed25d80869e08ea999a6edaa1bb1a10
telli <telli1@hotmail.fr>
2022-07-11 02:01:22 +0200

    Echo with option -n

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 20:27:11 by tmoragli          #+#    #+#             */
-/*   Updated: 2022/07/11 01:07:19 by tmoragli         ###   ########.fr       */
+/*   Updated: 2022/07/11 02:00:47 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,19 @@ int	check_echo_n(char *str)
 	int i;
 
 	i = 0;
-	if (str[i] != '-')
-		return (1);
+	if (str[i] != '-' && str[i + 1] != 'n')
+		return (0);
+	i = 1;
 	while (str && str[i])
 	{
-		if (str[i] != 'n')
-			return (1);
+		if (str[i] != 'n' && str[i] != '\0')
+			return (0);
 		i++;
 	}
-	return (0);
+	return (1);
 }
 
-int	ft_echo(int ac, char **av)
+int	echo(int ac, char **av)
 {
 	int	i;
 	int	new_line;
@@ -37,13 +38,18 @@ int	ft_echo(int ac, char **av)
 	i = 0;
 	if (ac == 1)
 		return(write(1, "\n", 1));
+	if (check_echo_n(av[0]))
+	{
+		new_line = 0;
+		while (av[i] && check_echo_n(av[i]))
+			i++;
+	}
 	while (av && av[i])
 	{
-		if (check_echo_n(av[i]))
-			ft_putstr_fd(av[i], 1);
-		else
-			new_line = 0;
+		ft_putstr_fd(av[i], 1);
 		i++;
+		if (av[i])
+			write(1, " ", 1);
 	}
 	if (new_line)
 		write(1, "\n", 1);
