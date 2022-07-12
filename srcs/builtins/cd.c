b4e5f9c2c2f418fdd30f7f7505f313ec7fc6d5ac
telli <telli1@hotmail.fr>
2022-07-12 22:02:16 +0200

    CD finished, .. added, -- added, - added, case with /path added and just folder_name functionality, errors messages added, to do : swap path and oldpath env variables for cd - and cd --

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,49 +6,58 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 19:24:29 by mdkhissi          #+#    #+#             */
-/*   Updated: 2022/07/11 22:58:10 by tmoragli         ###   ########.fr       */
+/*   Updated: 2022/07/12 22:00:22 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		is_double_dot(char *str)
+int		is_double_dash(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] && str[i] == '.')
+	while (str[i] && str[i] == '-')
 		i++;
 	if (i == 2 && str[i] == '\0')
-		return (1);
+		return (2);
+	if (i > 2 || (i == 2 && str[i]))
+		return (-1);
 	return (0);
 }
-
-char	*previous_dir(char *str)
+//----------TO DO BEFORE CD: CHANGE ENVIRONMENT VARIABLE : SWAP $OLDPWD and $PWD//
+char	*dash_dir(char *str, int ret)
 {
-	int		i;
+	if (ret == 1)
+		return (getenv("OLDPWD"));
+	if (ret == 2)
+		return (getenv("HOME"));
+	return (str);
+}
+
+int	is_dash(char	*str)
+{
+	int	i;
+	int ret;
 
 	i = 0;
-	while(str[i])
+	ret = is_double_dash(str);
+	while (str[i] && str[i] == '-')
 		i++;
-	i--;
-	while(str[i] != '/' && i != 0)
-	{
-		str[i] = 0;
-		i--;
-	}
-	return (str);
+	if (i == 1 && str[i] == '\0')
+		return (1);
+	return (ret);
 }
 
 int		change_path(char *str, char *path)
 {
+	if (is_double_dash(str) == -1)
+		return (printf("bash: --: invalid option\n"));
 	if (!str)
-	{
 		if (!getenv("HOME"))
 			return (printf("bash: cd: HOME not set\n"));
-	}
 	if (chdir(str) == -1)
-		return (-1);
+		return (printf("cd: %s: No such file or directory\n", str));
 	else
 		free(path);
 	return (1);
@@ -73,8 +82,8 @@ char	*concat_path(char *s1, char *s2)
 
 int		cd(int ac, char **str)
 {
-	char *path;
-	char *temp;
+	char	*path;
+	char	*temp;
 
 	path = pwd();
 	temp = path;
@@ -83,18 +92,21 @@ int		cd(int ac, char **str)
 		free(temp);
 		return (printf("bash: cd: too many arguments\n"));
 	}
+	printf("Path or folder given is [%s]\n", str[0]);
 	if (ac == 1)
 		return (change_path(getenv("HOME"), temp));
-	printf("Folder to go to is [%s]\n", str[0]);
-	if (is_double_dot(str[0]))
-		return (change_path(previous_dir(path), temp));
+	if (is_dash(str[0]) != 0)
+		return (change_path(dash_dir(str[0], is_dash(str[0])), temp));
+	if (str[0][0] == '/')
+		return (change_path(str[0], temp));
 	path = concat_path(path, str[0]);
+	printf("Path to go to is [%s]\n", path);
 	if (!path)
-		return (printf("allocation for path failed\n"));
+		return (printf("Allocation for path failed\n"));
 	if (change_path(path, path) == -1)
 	{
 		free(path);
-		return (printf("cd: %s: no such file or directory\n", str[0]));
+		return (printf("cd: %s: No such file or directory\n", str[0]));
 	}
 	return (1);
 }
