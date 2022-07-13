9c4fa6d8323c007889f204e14fc3d973e5b76d69
telli <telli1@hotmail.fr>
2022-07-13 17:28:30 +0200

    Start dev of exit, to do : norm and exit values to handle

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,16 +6,41 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 20:28:23 by tmoragli          #+#    #+#             */
-/*   Updated: 2022/07/11 16:16:18 by tmoragli         ###   ########.fr       */
+/*   Updated: 2022/07/13 17:27:35 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	check_digits(char **str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (str && str[i])
+	{
+		j = 0;
+		if (str[i][j] && (str[i][j] == '-' || str[i][j] == '+'))
+			i++;
+		while (str[i][j] && str[i][j] >= '0' && str[i][j] <= '9')
+			j++;
+		if (str[i][j] != '\0')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 int	shell_exit(int ac, char **av)
 {
-	(void)av;
-	if (ac != 2)
+	if (check_digits(av))
+	{
+		printf("exit\nbash: exit: %s: numeric argument required"))
+		exit(1);
+	}
+	if (ac > 1)
 		return (printf("bash: exit: too many arguments\n"));
-	exit(1);
+	printf("exit\n");
+	exit(ft_atoi(av[0]));
 }
