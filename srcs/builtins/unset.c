6a122238730b3aa3c5e3f801de6e823b7bcea565
Telli MORAGLIS <tmoragli@e1r3p15.clusters.42paris.fr>
2022-07-26 16:19:52 +0200

    Fixed unset leaks

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdkhissi <mdkhissi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 20:27:56 by tmoragli          #+#    #+#             */
-/*   Updated: 2022/07/26 15:06:33 by mdkhissi         ###   ########.fr       */
+/*   Updated: 2022/07/26 16:18:54 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,27 @@
 
 int ft_unset(t_data *data, int ac, char **av)
 {
-    int     i;
-    char    **to_del;
-    int     j;
-    char    **old_envr;
-    
-    j = 0;
-    to_del = malloc(ac * sizeof(char *));
-    i = 1;
-    while (av && av[i])
-    {
-        if (is_validid(av[i], ft_strlen(av[i])))
-        {
-            to_del[j] = ft_strjoin(av[i], "=");
-            j++;
-        }
-        i++;
-    }
-    to_del[j] = NULL;
-    old_envr = data->env_str;
-    data->env_str = str_arr_del(data->env_str, to_del, j);
-    str_arr_free(old_envr);
-    return (1);
+	int		i;
+	char	**to_del;
+	int		j;
+	char	**old_envr;
+
+	j = 0;
+	to_del = malloc(ac * sizeof(char *));
+	i = 1;
+	while (av && av[i])
+	{
+		if (is_validid(av[i], ft_strlen(av[i])))
+		{
+			to_del[j] = ft_strjoin(av[i], "=");
+			j++;
+		}
+		i++;
+	}
+	to_del[j] = NULL;
+	old_envr = data->env_str;
+	data->env_str = str_arr_del(data->env_str, to_del, j);
+	str_arr_free(to_del);
+	str_arr_free(old_envr);
+	return (1);
 }
