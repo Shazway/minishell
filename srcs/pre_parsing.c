9b684c6d768db9dcc318e753fb5307816606a53d
telli <telli1@hotmail.fr>
2022-07-28 14:14:57 +0200

    pre parsing modifs

--- srcs/pre_parsing.c
+++ srcs/pre_parsing.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 01:49:31 by tmoragli          #+#    #+#             */
-/*   Updated: 2022/07/28 14:09:22 by tmoragli         ###   ########.fr       */
+/*   Updated: 2022/07/28 14:12:32 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,9 +61,15 @@ int	check_input_pipe(t_data *data, int *i, int count)
 
 int	check_input(t_data *data)
 {
-	int	i;
-
+	int		i;
+	char	*tmp;
 	i = 0;
+
+	tmp = data->input;
+	data->input = ft_strtrim(data->input, " ");
+	free(tmp;)
+	if (!data->input)
+		msh_exit(data);
 	while (data->input && data->input[i])
 	{
 		if (data->input[i] == '>' || data->input[i] == '<')
