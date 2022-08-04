ac1f7ee40a9fa3a6ad87ba9aeb46b00d5f20dbbd
telli <telli1@hotmail.fr>
2022-08-04 03:14:26 +0200

    pwd small change

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 20:27:21 by tmoragli          #+#    #+#             */
-/*   Updated: 2022/07/28 18:24:21 by tmoragli         ###   ########.fr       */
+/*   Updated: 2022/08/04 03:13:57 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,18 @@ int	check_args_pwd(char **str)
 
 int	pwd(t_data *data, int ac, char **av)
 {
+	char	*pwd;
+
 	(void)ac;
 	if (!check_args_pwd(av))
 	{
 		ft_putstr_fd("minishell: pwd: --: invalid option\n", 2);
 		return (2);
 	}
-	if (!data->relative_path)
-		return (-1);
-	ft_printf("%s\n", data->relative_path);
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+		msh_exit(data, 1);
+	ft_printf("%s\n", pwd);
+	free(pwd);
 	return (0);
 }
